71111eca560e1d7b8dd64f4a6c9f7a0281bc0fc7
daya2940 <sauuit2940@gmail.com>
2022-04-11 00:16:49 +0530

    Crud api for category

---
+++ controller/category.js
@@ -0,0 +1,57 @@
+const Category = require("../models/category");
+const slugify = require("slugify");
+
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await new Category({ name, slug: slugify(name) }).save();
+    res.json(category);
+  } catch (err) {
+    res.status(400).send("Create category failed");
+  }
+};
+
+exports.list = async (req, res) => {
+  try {
+    const category = await Category.find({}.sort({ createdAt: -1 })).exec();
+    res.json(category);
+  } catch (err) {
+    res.status(400).send("category list not found");
+  }
+};
+
+exports.read = async (req, res) => {
+  try {
+    const category = await Category.findOne({ slug: req.params.slug }).exec();
+    res.json(category);
+  } catch (err) {
+    res.status(400).send(`Category${req.params.slug} not found`);
+  }
+};
+
+exports.update = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const category = await Category.findOneAndUpdate(
+      {
+        slug: req.params.slug,
+      },
+      { name, slug: slugify(name) },
+      { new: true }
+    ).exec();
+    res.json(category);
+  } catch (err) {
+    res.status(400).send("Category could not be  Updated");
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const category = await Category.findOneAndDelete({
+      slug: req.params.slug,
+    }).exec();
+    res.json(category);
+  } catch (err) {
+    res.status(400).send("Create category failed");
+  }
+};
---
+++ models/category.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Name is required",
+      minlength: [3, "Too short"],
+      maxlength: [32, "Too long"],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
